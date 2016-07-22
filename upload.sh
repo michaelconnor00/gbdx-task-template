3008eaa88818fca9050599c4200bc705d4c75337
Michael Connor <mike@sparkgeo.com>
2016-07-22 15:40:29 -0700

    Fix pypirc creation overriding heading.

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 echo "[pypi]" > ~/.pypirc
-echo "repository=https://pypi.python.org/pypi" > ~/.pypirc
-echo "username:" $PYPI_USER >> ~/.pypirc
+echo "repository=https://pypi.python.org/pypi" >> ~/.pypirc
+echo "username:" $PYPI_USERNAME >> ~/.pypirc
 echo "password:" $PYPI_PASSWORD >> ~/.pypirc
 cat ~/.pypirc
 python setup.py sdist upload
