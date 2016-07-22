4cda279b7e2d02404de7af814e6b77f737b6fb90
Michael Connor <mike@sparkgeo.com>
2016-07-22 15:38:11 -0700

    Revise pypirc creation.

--- upload.sh
+++ upload.sh
@@ -1,4 +1,6 @@
-echo "[server-login]" > ~/.pypirc
+echo "[pypi]" > ~/.pypirc
+echo "repository=https://pypi.python.org/pypi" > ~/.pypirc
 echo "username:" $PYPI_USER >> ~/.pypirc
 echo "password:" $PYPI_PASSWORD >> ~/.pypirc
+cat ~/.pypirc
 python setup.py sdist upload
