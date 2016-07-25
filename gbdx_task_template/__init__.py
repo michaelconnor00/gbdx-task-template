d4ababef5d4efca503b85466771abc5d8f82380b
Michael Connor <mike@sparkgeo.com>
2016-07-25 14:15:20 -0700

    Change init to suit new package name.

---
+++ gbdx_task_template/__init__.py
@@ -0,0 +1,3 @@
+from gbdx_task_template.base import TaskTemplate
+from gbdx_task_template.task import Task
+from gbdx_task_template.port import InputPort, OutputPort
--- task_template/__init__.py
+++
@@ -1,3 +0,0 @@
-from task_template.base import TaskTemplate
-from task_template.task import Task
-from task_template.port import InputPort, OutputPort
