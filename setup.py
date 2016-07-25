8ebf9a31eee760d3b39de8c578d7fcc3df265fd1
Michael Connor <mike@sparkgeo.com>
2016-07-25 13:57:21 -0700

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gbdx-task-template',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
 
     include_package_data=True,
